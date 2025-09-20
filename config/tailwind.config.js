64c624da7fc60c39e746083bf5f9b7e92d0021ec
Wellyson-Lopes <wellyson.dev@gmail.com>
2025-09-20 13:59:04 -0300

    Start application truck manager

---
+++ config/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+ darkMode: "class",
+ content: [
+     "./public/*.html",
+     "./app/helpers/**/*.rb",
+     "./app/javascript/**/*.js",
+     "./app/views/**/*.{erb,haml,html,slim}",
+ ],
+ theme: {
+     extend: {
+         fontFamily: {
+             sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+         }
+     }
+ },
+ plugins: [
+     require("@tailwindcss/forms"),
+     require("@tailwindcss/typography"),
+     require("@tailwindcss/container-queries")
+ ]
+}
